281609ff5adaa08c5673ac89dd288a97b91661a0
rebornszoro <reborns-ou@hotmail.com>
2019-04-08 14:57:02 +0800

    use better-scroller to store city-list and add alphabet display of city

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <ul class="alphabetList">
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../../assets/styles/varibles.styl'
+  .alphabetList
+    position: absolute
+    display: flex
+    flex-direction: column
+    top: 1.6rem
+    bottom: 0
+    right: .1rem
+    justify-content: center
+    .item
+      line-height: .4rem
+      width: .4rem
+      text-align: center
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">中山</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">中山</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">中山</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">中山</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">中山</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">中山</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../../assets/styles/varibles.styl'
+  .list
+    position: absolute
+    top: 1.6rem
+    right: 0
+    bottom: 0
+    left: 0
+    // background: red
+    overflow: hidden
+    .border-topbottom
+      &:before
+        border-color: #ccc
+      &:after
+        border-color: #ccc
+    .border-bottom
+      &:before
+        border-color: #ccc
+    .title
+      height: .44rem
+      line-height: .44rem
+      background: #eee
+      padding-left: .2rem
+      color: #666
+      font-size: .26rem
+    .button-list
+      padding: .1rem .6rem .1rem .1rem// 预留给右侧的导航栏
+      overflow: hidden
+      .button-wrapper
+        width: 33.33%
+        float: left
+        .button
+          margin: .1rem
+          padding: .1rem 0
+          text-align: center
+          border: .02rem solid #ccc
+          border-radius: .06rem
+    .item-list
+      .item
+        line-height: .76rem
+        color: #666
+        padding-left: .2rem
+  </style>
