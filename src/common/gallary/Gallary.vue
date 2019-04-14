ca0cc9a661e24d2c7b7755127533357f0b79b2bf
rebornszoro <reborns-ou@hotmail.com>
2019-04-14 17:39:43 +0800

    build gallary demo,gallary will display when banner of detail is clicked

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper" @click="handleWrapperClicked">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide v-for="(item, index) in imgList" :key="index">
+        <div class="img-wrapper">
+          <img class="img" :src="item" />
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgList: {
+      type: Array,
+      default () {
+        return [
+          'http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg',
+          'http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg'
+        ]
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer: true,
+        observeParents: true
+      }
+    }
+  },
+  methods: {
+    handleWrapperClicked () {
+      this.$emit('handleWrapperClicked')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-container
+    overflow: inherit
+    width: 100%
+  .wrapper
+    z-index: 99
+    position: fixed
+    top: 0
+    right: 0
+    bottom: 0
+    left: 0
+    background: #000
+    display: flex
+    flex-direction: column
+    justify-content: center
+    .img-wrapper
+      height: 0
+      padding-bottom: 55%
+      .img
+        width: 100%
+    .swiper-pagination
+      position: relative
+      top: .5rem
+      // background: #fff
+      width: 100%
+      color: #999
+</style>
--- src/pages/details/components/DetailsBanner.vue
+++ src/pages/details/components/DetailsBanner.vue
@@ -1,19 +1,50 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg">
-    <div class="banner-info">
-      <div class="banner-title">长隆野生动物世界(5A景区)</div>
-      <div class="banner-img-counts">
-        102
-        <span class="iconfont">&#xe6dd;</span>
+  <div>
+    <div class="banner">
+      <img
+        class="banner-img"
+        @click="handleGallaryClicked"
+        src="//img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg">
+      <div class="banner-info">
+        <div class="banner-title">长隆野生动物世界(5A景区)</div>
+        <div class="banner-img-counts">
+          <span class="count">102</span>
+          <span class="iconfont">&#xe6dd;</span>
+        </div>
       </div>
     </div>
+    <common-gallary
+      :imgList="imgList"
+      v-show="showGallary"
+      @handleWrapperClicked="handleWrapperClicked"
+    ></common-gallary>
   </div>
 </template>
 
 <script>
+import CommonGallary from 'common/gallary/Gallary.vue'
 export default {
-  name: 'DetailsBanner'
+  name: 'DetailsBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgList: [
+        'http://img1.qunarzz.com/sight/p0/1605/d4/d4e2dce9d328d5b290.img.jpg_r_800x800_892973df.jpg',
+        'http://img1.qunarzz.com/sight/p0/1605/d4/d4e2dce9d328d5b290.img.jpg_r_800x800_892973df.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleGallaryClicked () {
+      this.showGallary = true
+    },
+    handleWrapperClicked () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 
@@ -43,7 +74,12 @@ export default {
       .banner-img-counts
         float: right
         margin-right: .12rem
-        // background: rgba(0, 0, 0, 0.28)
-        // border-radius: 0.15rem
-        // padding: 0 .1rem
+        line-height: .64rem
+        height: .64rem
+        display: flex
+        .count
+          font-size: .3rem
+          padding: 0 .1rem
+        .iconfont
+          font-size: .4rem
 </style>
